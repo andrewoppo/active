a569e813c7846c22e2d0d840afa1a7526ff1f25b
Andrew Oppo <andrew.oppo@gmail.com>
2021-07-29 15:16:58 +0200

    fix for heroku

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ passport.use(
   
 app.use(passport.initialize());
 app.use(passport.session());
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
   
 
 // 👇 Start handling routes here
